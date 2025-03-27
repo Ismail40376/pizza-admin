deac1aef0a7de1986a4ebe022fbd8e34e8f127dd
Ismail <dlatiktoka304@gmail.com>
2025-03-27 12:42:40 +0500

    create Dish Item, Dish Form, add redux toolkit

---
+++ src/pages/DishesPage/mock.ts
@@ -0,0 +1,22 @@
+export const dishesData = [
+    {
+        id: 1,
+        title: "Pepperoni",
+        price: 2800,
+        img: "/pepperoni.jpeg"
+    },
+
+    {
+        id: 2,
+        title: "Margarita",
+        price: 2000,
+        img: "/margarita.jpeg"
+    },
+
+    {
+        id: 3,
+        title: "Hawaiian",   
+        price: 3800,
+        img: "/hawaiian-pizza.jpg"
+    },
+]
